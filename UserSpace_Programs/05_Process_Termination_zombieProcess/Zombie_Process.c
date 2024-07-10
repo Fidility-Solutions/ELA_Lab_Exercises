7dc630510ad78ce28cd118a4326e5310809acf38
Rajanikanth-chevula <rajanikanth.chevula@fidilitysolutions.com>
2024-07-10 14:49:09 +0530

    Renamed folder 04_Orphan_Process to 04_Process_Termination_orphanProcess and 05_Zombie_Process to 05_Process_Termination_zombieProcess

---
+++ UserSpace_Programs/05_Process_Termination_zombieProcess/Zombie_Process.c
@@ -0,0 +1,79 @@
+/********************************************************************************
+ * File		: Zombie_Process.c
+ *
+ * Description	: This file contains a C program demonstrating the concept of a zombie process. A zombie process 
+ *              occurs when a child process completes execution, but its exit status has not yet been retrieved 
+ *              by its parent process. 
+ *              In this program, a parent process forks a child process. The child process immediately exits, 
+ *              while the parent process waits for a short time before exiting. 
+ *              As a result, the child process becomes a zombie until the parent process retrieves its exit 
+ *              status using the wait() system call.
+ *
+ * Author	: Fidility Solutions.
+ *
+ * Date	  	: 23/02/2024.
+ *
+ * Reference   	: The "Linux Programming Interface" book.
+ ********************************************************************************/
+
+#include <signal.h>
+#include <libgen.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#define CMD_SIZE 200
+
+
+/*
+ * Function     : main()
+ *
+ * Description  : Entry point of the program. Demonstrates the concept of an zombie process.
+ *
+ * Parameters   : None
+ *
+ * Returns      : 0 on successful execution, 1 on failure.
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("This program demonstrates how process become zombie\n");
+	char cmd[CMD_SIZE];
+	pid_t childPid;
+	setbuf(stdout, NULL);
+	/* Disable buffering of stdout */
+	printf("Parent PID=%ld\n", (long) getpid());
+	sleep(5);
+	switch (childPid = fork()) {
+		case -1:
+			perror("fork error");
+			exit(EXIT_FAILURE);
+		case 0:
+		/* Child: immediately exits to become zombie */
+			printf("Child (PID=%ld) exiting\n", (long) getpid());
+			exit(EXIT_SUCCESS);
+		default:	/* Parent */
+			sleep(3);
+			/* Give child a chance to start and exit */
+			snprintf(cmd, CMD_SIZE, "ps | grep %s", basename(argv[0]));
+			cmd[CMD_SIZE - 1] = '\0';
+			/* Ensure string is null-terminated */
+			printf("running ps command to check process entry in process table\n");
+			system(cmd);
+			/* View zombie child */
+			/* Now send the "sure kill" signal to the zombie */
+			if (kill(childPid, SIGKILL) == -1){
+					perror("failed to kill child");
+					exit(EXIT_FAILURE);
+			}
+			printf("sent kill signal to zombie process\n");
+			sleep(3);
+			/* Give child a chance to react to signal */
+			printf("After sending SIGKILL to zombie (PID=%ld):\n", (long) childPid);
+			system(cmd);
+			/* View zombie child again */
+			exit(EXIT_SUCCESS);
+	}
+}
