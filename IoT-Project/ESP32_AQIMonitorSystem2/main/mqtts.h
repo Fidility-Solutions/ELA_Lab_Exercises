12fa649a2da96043e208224ec9343eeee04e0715
Rajanikanth.chevula <rajanikanth.chevula@fidilitysolutions.com>
2025-04-24 22:32:01 +0530

    Fixed bugs in control command operations and implemented mechanism for reading and storing aws keys to/from eeprom

--- IoT-Project/ESP32_AQIMonitorSystem2/main/mqtts.h
+++ IoT-Project/ESP32_AQIMonitorSystem2/main/mqtts.h
@@ -7,7 +7,6 @@
 #define MQTTS_TAG	"MQTT"
 
 
-extern volatile uint32_t u32SetPublishInterval;
 extern uint8_t u8CloudConnect;
 /* Declare the mqtt config (but don't initialize it here) */
 extern esp_mqtt_client_config_t mqtt_cfg;
